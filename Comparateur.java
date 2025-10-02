1c8038c9016027f1a4d7c40c12e329ef13bde420
Erkin Tunc <77968721+ErkinTunc@users.noreply.github.com>
2025-10-02 23:37:50 +0200

    Update Comparateur.java

--- Comparateur.java
+++ Comparateur.java
@@ -1,17 +1,46 @@
 import java.io.*;
 import java.util.*;
 
-// compare deux nuplets par rapport à l'ordre lexicographique d'un sous-ensemble de ces indices
-// attention, l'ordre des indices compte et il n'est pas forcément croissant
+/**
+ * Le comparateur permet de définir un ordre sur des n-uplets en fonction d’une
+ * sous-liste de leurs colonnes.
+ * 
+ * Exemple : si indices = [1, 3], alors on compare d'abord la colonne 1, puis
+ * la colonne 3 si nécessaire.
+ * 
+ * Attention : l'ordre des indices donné par l'utilisateur compte et n'est pas
+ * forcément croissant.
+ */
 class Comparateur implements Comparator<String[]> {
 
     public final int[] indices;
-    
+
+    /**
+     * Constructeur du comparateur
+     * 
+     * @param indices les indices des colonnes sur lesquelles faire la comparaison
+     */
     public Comparateur(int[] indices) {
         this.indices = indices;
     }
 
+    /**
+     * Compare deux n-uplets en fonction des colonnes spécifiées dans indices
+     * - l’ordre lexicographique = dictionary order (a-z)
+     * 
+     * @param t1 le premier n-uplet
+     * @param t2 le second n-uplet
+     * 
+     * @return un entier négatif, zéro ou positif si t1 est respectivement
+     *         inférieur, égal ou supérieur à t2
+     */
     public int compare(String[] t1, String[] t2) {
-        return 0;
+        for (int index : indices) {
+            int compare = t1[index].compareTo(t2[index]);
+            if (compare != 0) {
+                return compare; // dès qu'on trouve une différence on la retourne
+            }
+        }
+        return 0; // toutes les colonnes testées sont égales
     }
 }
